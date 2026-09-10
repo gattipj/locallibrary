680e3cca08084f6a4ee7a579d9921bd53fee2dc9
Philip Gatti <gattipj@dukes.jmu.edu>
2026-09-09 23:00:11 -0400

    yo

--- catalog/admin.py
+++ catalog/admin.py
@@ -18,6 +18,7 @@ admin.site.register(Language)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInstanceInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -43,3 +44,4 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
+    list_display = ('book', 'status', 'due_back', 'id')
