ddaa58b3892694d92af532e8f87fe1a51878c6b5
Philip Gatti <gattipj@dukes.jmu.edu>
2026-09-16 22:40:46 -0400

    almost done!

--- catalog/views.py
+++ catalog/views.py
@@ -8,7 +8,7 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    num_genres = Genre.objects.all().count()
+    # num_genres = Genre.objects.filter(__str__="yes").count()
 
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
@@ -24,7 +24,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_genres': num_genres,
+        # 'num_genres': num_genres,
         # 'num_books_word': num_books_word,
     }
 
